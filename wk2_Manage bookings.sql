5bd1fff76d23546f00e36fd46524d48128667463
Patricia O. Idakwo <7622299+PatUnoka@users.noreply.github.com>
2023-07-13 15:54:18 +0100

    more week2  exercises

---
+++ wk2_Manage bookings.sql
@@ -0,0 +1,13 @@
+USE LittleLemonDB;
+DROP PROCEDURE IF EXISTS UpdateBooking;
+DELIMITER //
+CREATE PROCEDURE UpdateBooking(IN mybookingid INT, IN mybookingdate DATE)
+BEGIN
+UPDATE Bookings SET Date = mybookingdate WHERE BookingID = mybookingid;
+SELECT CONCAT("Booking ", mybookingid, " updated") AS "Confirmation";
+END //
+DELIMITER ;
+
+CALL UpdateBooking(3, 1, "2022-12-17");
+
+SELECT * FROM Bookings;
